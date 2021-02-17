1e28fb515d5c389785313d7565dd5480f93744f7
turavinin <aturavinin@gmail.com>
2021-02-17 12:13:46 -0300

    Add project files.

---
+++ PortfolioV3/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+const hamBtn = document.querySelector('.hamburger');
+const hdrText = document.querySelector('.header__text');
+const header = document.querySelector('.header');
+
+hamBtn.addEventListener('click', () => {
+  hdrText.classList.toggle('change');
+  header.classList.toggle('change');
+});
