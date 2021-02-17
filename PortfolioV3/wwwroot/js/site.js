3acaf42b770bf1b6633e7ba64c94b09f815ff22a
turavinin <aturavinin@gmail.com>
2021-02-17 14:00:51 -0300

    fix mobile navbar

--- PortfolioV3/wwwroot/js/site.js
+++ PortfolioV3/wwwroot/js/site.js
@@ -5,8 +5,16 @@
 const hamBtn = document.querySelector('.hamburger');
 const hdrText = document.querySelector('.header__text');
 const header = document.querySelector('.header');
+const navBarItem = document.querySelectorAll('.navbar__item');
 
 hamBtn.addEventListener('click', () => {
   hdrText.classList.toggle('change');
   header.classList.toggle('change');
 });
+
+navBarItem.forEach((e) =>
+  e.addEventListener('click', () => {
+    hdrText.classList.remove('change');
+    header.classList.remove('change');
+  })
+);
